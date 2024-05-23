15b248a81f93bf9faa54153d7e4ba14d7b36c656
Macapps13 <ben.alvaro@icloud.com>
2024-05-24 08:30:53 +1000

    modified:   main.py 	deleted:    padlock (1).png

--- main.py
+++ main.py
@@ -7,16 +7,38 @@ import Keylogger
 import Network
 import Encryptor
 import time
+from tkinter import messagebox as msg
 #Imports the appropriate libraries for GUI interface
 #Also imports other files to ensure efficiency and prevent project from being too big
 
+
+def on_closing():
+    response = msg.askyesnocancel("Confirm Exit", "Are you sure you want to exit?")
+    if response is None:
+        # User clicked "Cancel"
+        return
+    elif response:
+    # Close the application
+        exit(0)
+
+
 def openMenu():
-    app.destroy()
+    app.withdraw()
+    app.quit()
     menu = CTk()
     menu.geometry("500x400")
     menu.maxsize(500, 400)
     menu.minsize(500,400)
+    menu.title("MHS Sybersecurity Management Platform")
+    quitBtn2 = CTkButton(master=menu, text="Exit", corner_radius=10, fg_color="transparent", 
+                     hover_color="#598eb2", border_color="#FFFFFF", border_width=2, command=on_closing)
+    quitBtn2.place(relx=0.5, rely=0.85, anchor="center")
+
+
+    menu.protocol("WM_DELETE_WINDOW", on_closing)
     menu.mainloop()
+
+
 
 
 app = CTk()
@@ -32,38 +54,45 @@ passwordImg_data = Image.open("password.png")
 userImg = CTkImage(dark_image=userImg_data)
 passwordImg = CTkImage(dark_image=passwordImg_data)
 
-usernameLabel = CTkLabel(master=app, text="  Username:", anchor="w", justify="left", font=("Arial Bold", 14), image=userImg, compound="left")
+usernameLabel = CTkLabel(master=app, text="  Username:", anchor="w", justify="left", font=("Arial Bold", 14), 
+                         image=userImg, compound="left")
 usernameLabel.place(relx=0.275, rely= 0.32, anchor="w")
-usernameEntry = CTkEntry(master=app, width=225, fg_color="#EEEEEE", border_color="#598eb2", border_width=1, text_color="#000000")
+usernameEntry = CTkEntry(master=app, width=225, fg_color="#EEEEEE", 
+                         border_color="#598eb2", border_width=1, text_color="#000000")
 usernameEntry.place(relx=0.5, rely=0.4, anchor="center")
 
 
-passwordLabel = CTkLabel(master=app, text="  Password:", anchor="w", justify="left", font=("Arial Bold", 14), image=passwordImg, compound="left")
+passwordLabel = CTkLabel(master=app, text="  Password:", anchor="w", justify="left", 
+                         font=("Arial Bold", 14), image=passwordImg, compound="left")
 passwordLabel.place(relx=0.275, rely= 0.52, anchor="w")
-passwordEntry = CTkEntry(master=app, width=225, fg_color="#EEEEEE", border_color="#598eb2", border_width=1, text_color="#000000", show="*")
+passwordEntry = CTkEntry(master=app, width=225, fg_color="#EEEEEE", border_color="#598eb2", 
+                         border_width=1, text_color="#000000", show="*")
 passwordEntry.place(relx=0.5, rely=0.6, anchor="center")
 
-unsuccessfulLogInMessage = CTkLabel(master=app, text=" ", text_color="#EE4B2B", anchor="center", font=("ArialBold", 12))
-unsuccessfulLogInMessage.place(relx=0.5, rely=0.9, anchor="center")
+unsuccessfulLogInMessage = CTkLabel(master=app, text=" ", text_color="#EE4B2B", 
+                                    anchor="center", font=("ArialBold", 12))
+unsuccessfulLogInMessage.place(relx=0.5, rely=0.7, anchor="center")
 
-def logInPressed():
+def logInPressed(e):
     enteredUsername = usernameEntry.get()
     enteredPword = passwordEntry.get()
     print("enteredUsername = " + enteredUsername + "\nenteredPword = " + enteredPword)
-    if enteredUsername == "admin" and enteredPword == "Merewether2024":
-        openMenu()
-    else:
+    if enteredUsername != "admin" or enteredPword != "Merewether2024":
         print("Unsuccessful")
         unsuccessfulLogInMessage.configure(text="Unsuccessful Login Attempt, Please Try Again")
         app.update_idletasks()
-        app.after(5000, unsuccessfulLogInMessage.configure(text=" "))
+    else:
+        openMenu()
+
         
         
 
 
 
-welcomeLabel = CTkLabel(master=app, text="Welcome to the MHS Cybersecurity Dashboard", font=("Calibri", 20))
-instructionLabel = CTkLabel(master=app, text="Please log in with your administor credentials", font=("Calibri", 20))
+welcomeLabel = CTkLabel(master=app, text="Welcome to the MHS Cybersecurity Dashboard", 
+                        font=("Calibri", 20))
+instructionLabel = CTkLabel(master=app, text="Please log in with your administor credentials", 
+                            font=("Calibri", 20))
 #Creates labels for welcome message and assigns properties
 
 welcomeLabel.place(relx=0.5, rely=0.1, anchor="center")
@@ -72,15 +101,18 @@ instructionLabel.place(relx=0.5, rely=0.2, anchor="center")
 
 logInBtn = CTkButton(master=app, text="Log In", corner_radius=10, fg_color="transparent", 
                      hover_color="#598eb2", border_color="#FFFFFF", border_width=2, command=logInPressed)
-logInBtn.place(relx=0.5, rely=0.8, anchor="center")
+logInBtn.place(relx=0.3, rely=0.85, anchor="center")
 #Creates and places the login button
 
+quitBtn = CTkButton(master=app, text="Exit", corner_radius=10, fg_color="transparent", 
+                     hover_color="#598eb2", border_color="#FFFFFF", border_width=2, command=on_closing)
+quitBtn.place(relx=0.7, rely=0.85, anchor="center")
 
-def on_closing():
-    print("Exited with code 0")
-    app.destroy()
 
 app.protocol("WM_DELETE_WINDOW", on_closing)
 
+app.bind('<Return>',logInPressed)
+
+app.title("MHS Sybersecurity Management Platform")
 app.mainloop()
 #Opens the window
