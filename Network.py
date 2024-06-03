0fb941ca975e8d3b00bc989022f10560ade74d86
Macapps13 <ben.alvaro@icloud.com>
2024-06-03 10:06:32 +1000

    modified:   Network.py

--- Network.py
+++ Network.py
@@ -10,3 +10,46 @@ from queue import Queue
 target = "153.107.45.15" ##MHS IP Address
 queue = Queue()
 open_ports = []
+
+def portscan(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        return True
+    except:
+        return False
+    
+def get_ports(mode):
+    if mode == 1:
+        for port in range(1, 1024):
+            queue.put(port)
+    elif mode == 2:
+        for port in range(1, 49152):
+            queue.put(port)
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        if portscan(port):
+            print("Uh Oh! Port {} is open!".format(port))
+            open_ports.append(port)
+        else:
+            print("Port {} is closed!".format(port))
+def run_scanner(threads, mode):
+
+    get_ports(mode)
+
+    thread_list = []
+
+    for t in range(threads):
+        thread = threading.Thread(target=worker)
+        thread_list.append(thread)
+
+    for thread in thread_list:
+        thread.start()
+
+    for thread in thread_list:
+        thread.join()
+
+    print("Open ports are:", open_ports)
+
+run_scanner(100, 1)
